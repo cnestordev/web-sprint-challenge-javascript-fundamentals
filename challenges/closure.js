8d244d150eb5491e756230e93e7eec18a73e2884
Nestor <ncervan87@gmail.com>
2020-06-19 10:28:04 -0700

    finish MVP

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,22 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// The reason why the nested function can access the 'internal' variable is due to closure.  The nested function has closure, which gives it access to the functions
+//that exists in outter scopes.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(number) {
+  let finalNumber = 0
+  function counter() {
+    while (number > 0) {
+      finalNumber += number--
+    }
+    return finalNumber
+  }
+  return counter()
+}
+
+
